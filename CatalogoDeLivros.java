337b08347e434d41b9a10629aac98441faf5fff5
jaibento <jaine2017bento@gmail.com>
2024-09-11 12:45:10 -0300

    Add files via upload

---
+++ CatalogoDeLivros.java
@@ -0,0 +1,69 @@
+
+package projetoads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoDeLivros {
+    private List<Book> books;
+
+    public CatalogoDeLivros() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getAvailableBooks() {
+        List<Book> availableBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (book.isAvailable()) {
+                availableBooks.add(book);
+            }
+        }
+        return availableBooks;
+    }
+
+    public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
+
+class Book {
+    String title;
+    String author;
+    boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void borrowBook() {
+        isAvailable = false;
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+    }
+}
+
