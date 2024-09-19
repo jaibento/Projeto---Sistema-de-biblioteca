cfb6fea4279f1c2ff793c9d535942de0ba37dc85
jaibento <jaine2017bento@gmail.com>
2024-09-19 19:52:47 -0300

    Create Data.sql

---
+++ Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (title, author) VALUES ('1984', 'George Orwell');
+INSERT INTO book (title, author) VALUES ('Brave New World', 'Aldous Huxley');
+INSERT INTO book (title, author) VALUES ('Fahrenheit 451', 'Ray Bradbury');
